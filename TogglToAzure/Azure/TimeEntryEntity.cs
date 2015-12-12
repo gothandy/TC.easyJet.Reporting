86f8334f861a31e62c0c49ea7b6f6a3563cdd983
gothandy <gothandy@hotmail.com>
2015-12-12 19:13:47 +0000

    Pushing Toggl data to Azure

---
+++ TogglToAzure/Azure/TimeEntryEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace TC.easyJet.Reporting
+{
+    public class TimeEntryEntity : TableEntity
+    {
+
+        public TimeEntryEntity(long? taskId, long? id)
+        {
+
+            this.PartitionKey = taskId.ToString();
+            this.RowKey = id.ToString();
+        }
+
+        public TimeEntryEntity() { }
+
+        public long? ProjectId { get; set; }
+        public DateTime? Start { get; set; }
+        public string TaskName { get; set; }
+        public string UserName { get; set; }
+        public long? Billable { get; set; }
+        
+    }
+}
---
+++ TogglToAzure/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Toggl.DataObjects;
+using Toggl;
+using System.Collections.Generic;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace TC.easyJet.Reporting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var apiKey = "242d8528ee1e461cdda80cb6eb175967";
+            var connectionString = "DefaultEndpointsProtocol=https;AccountName=tceasyjetreporting;AccountKey=XWTDcIlFnS5ZHdaW0bhBvADcAtGlSQgBYlfeeYcynSeJaFRBnRCDp4nqRJUAymjHTSyGRvpDkqYMG7AcE+tvWw==";
+
+            var workspaceId = 605632;
+            var clientId = 15242883;
+
+            var since = new DateTime(2015, 1, 1);
+            var until = new DateTime(2015, 12, 31);
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            CloudTable table = tableClient.GetTableReference("TimeEntries");
+
+            //table.Delete();
+            table.CreateIfNotExists();
+
+            var workspace = new Workspace(apiKey, workspaceId);
+
+            List<ReportTimeEntry> reportTimeEntries = workspace.GetReportTimeEntries(clientId, since, until);
+
+            foreach (ReportTimeEntry timeEntry in reportTimeEntries)
+            {
+
+                TimeEntryEntity entity = new TimeEntryEntity(timeEntry.TaskId, timeEntry.Id);
+
+                entity.ProjectId = timeEntry.ProjectId;
+                entity.TaskName = timeEntry.TaskName;
+                entity.Start = timeEntry.Start;
+                entity.UserName = timeEntry.UserName;
+                entity.Billable = timeEntry.Billable;
+
+                TableOperation operation = TableOperation.InsertOrReplace(entity);
+
+                table.Execute(operation);
+            }
+        }
+    }
+}
