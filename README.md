12e4196c781092222d336042bf2e2771231a568c
gothandy <gothandy@hotmail.com>
2015-12-13 14:00:26 +0000

    Getting Cards data into dynamic object.

--- README.md
+++ README.md
@@ -3,6 +3,16 @@
 ##Command Line Arguments
 AzureMapping.exe AzureStorageAccountKey
 TogglToAzure.exe AzureStorageAccountKey ToggleApiKey
+TrelloToAzure.exe AzureStorageAccountKey TrelloToken
+ 
+##AzureStorageAccountKey
+Goto https://portal.azure.com/ and find the tceasyjetreporting storage account. You will find primary and secondary keys there.
+
+##ToggleApiKey
+Goto https://www.toggl.com/app/profile your personal key is found at the bottom of the page.
+
+##TrelloToken
+The public Key is 3ba00ca224256611c3ccbac183364259, use this on the sandbox https://developers.trello.com/sandbox to allow access from your account to this application. You'll need Chrome with Developer tools running to watch the Network traffic and grab the token. Good tool for exploring calls to the API too.
 
 ##Debug
 For the Console Applications Command Line Arguments need adding to the Debug section of the project Properties where they get added to your .csporj.user files so only stored locally.
---
+++ TrelloToAzure/Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrelloToAzure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var accountKey = args[0];
+
+            var connectionString = String.Format(
+                "DefaultEndpointsProtocol=https;AccountName=tceasyjetreporting;AccountKey={0}",
+                accountKey);
+
+            var trelloKey = "3ba00ca224256611c3ccbac183364259";
+            var trelloToken = args[1];
+            var boardId = "5596a7b7ac88c077383d281c";
+            var fields = "name,idList,idLabels";
+
+            var url = String.Format(
+                "https://api.trello.com/1/boards/{0}/cards?fields={1}&key={2}&token={3}",
+                boardId, fields, trelloKey, trelloToken);
+
+            using (var webClient = new WebClient())
+            {
+                webClient.Headers.Add("Content-Type", "application/json");
+                var json = webClient.DownloadString(url);
+
+                var cards = JsonConvert.DeserializeObject(json);
+            }
+        }
+    }
+}
