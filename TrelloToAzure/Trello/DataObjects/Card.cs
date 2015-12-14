ef82a1a1c675c3e22a5a1534302e431c2af2e5f4
gothandy <gothandy@hotmail.com>
2015-12-14 11:31:27 +0000

    Missing file.

---
+++ TrelloToAzure/Trello/DataObjects/Card.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace TrelloToAzure.Trello.DataObjects
+{
+    public class Card
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "idList")]
+        public string IdList { get; set; }
+
+        [JsonProperty(PropertyName = "idLabels")]
+        public List<string> IdLabels { get; set; }      
+
+    }
+}
