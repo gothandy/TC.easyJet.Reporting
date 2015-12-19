1deb7576802e34818e56e4db8c40b0da8b0eb434
gothandy <gothandy@hotmail.com>
2015-12-19 19:58:36 +0000

    Orderbys and extra logic in linq.

--- WebApp/Controllers/DefaultController.cs
+++ WebApp/Controllers/DefaultController.cs
@@ -1,6 +1,8 @@
 ﻿using Azure;
 using Azure.Entities;
 using Azure.Tables;
+using System.Collections;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Web.Mvc;
@@ -23,18 +25,21 @@ namespace WebApp.Controllers
             TimeEntryTable timeEntryTable = new TimeEntryTable(client);
             CardTable cardTable = new CardTable(client);
 
-            var join = from timeEntry in timeEntryTable.Query()
+            var join = from timeEntry in GroupByMonth(timeEntryTable.Query())
                         join card in cardTable.Query()
                         on timeEntry.DomId equals card.DomId into g
                         from card2 in g.DefaultIfEmpty()
+                        orderby
+                            (card2 == null ? timeEntry.Month : card2.Invoice),
+                            (card2 == null ? timeEntry.HouseKeeping : card2.DomId)
                         select new JoinModel()
                         {
                             DomId = timeEntry.DomId,
                             HouseKeeping = timeEntry.HouseKeeping,
                             List = (card2 == null ? null: card2.List),
                             Name = (card2 == null ? null: card2.Name),
-                            Epic = (card2 == null ? null: card2.Epic),
-                            Invoice = (card2 == null ? null: card2.Invoice),
+                            Epic = (card2 == null ? "HouseKeeping": card2.Epic),
+                            Invoice = (card2 == null ? timeEntry.Month: card2.Invoice),
                             Month = timeEntry.Month,
                             UserName = timeEntry.UserName,
                             Billable = timeEntry.Billable
@@ -60,18 +65,25 @@ namespace WebApp.Controllers
 
             TimeEntryTable table = new TimeEntryTable(client);
 
-            var query = table.Query();
+            var result = GroupByMonth(table.Query());
 
+            return View(result);
+        }
+
+        private static IEnumerable<TimeEntryEntity> GroupByMonth(IEnumerable<TimeEntryEntity> query)
+        {
             var result =
                 from e in query
-                group e by new {
+                group e by new
+                {
                     e.Month,
                     e.UserName,
                     e.DomId,
                     e.HouseKeeping
 
                 } into g
-                select new TimeEntryEntity() {
+                select new TimeEntryEntity()
+                {
                     Month = g.Key.Month,
                     UserName = g.Key.UserName,
                     DomId = g.Key.DomId,
@@ -79,7 +91,7 @@ namespace WebApp.Controllers
                     Billable = g.Sum(e => e.Billable)
                 };
 
-            return View(result);
+            return result;
         }
 
         private static TableClient GetNewTableClient()
