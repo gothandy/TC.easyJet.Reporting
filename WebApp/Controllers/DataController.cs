7e251e36dc0e21a139c29ac38858693138cc1716
gothandy <gothandy@hotmail.com>
2015-12-22 09:17:00 +0000

    Updated urls ready for invoices and wip.

---
+++ WebApp/Controllers/DataController.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web.Mvc;
+using Vincente.Azure;
+using Vincente.Azure.Entities;
+using Vincente.Azure.Tables;
+using Vincente.WebApp.Models;
+
+namespace Vincente.WebApp.Controllers
+{
+    public class DataController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AllByMonth()
+        {
+            TableClient client = GetNewTableClient();
+
+            TimeEntryTable timeEntryTable = new TimeEntryTable(client);
+            CardTable cardTable = new CardTable(client);
+
+            var stories =
+                from timeEntry in GroupByMonth(timeEntryTable.Query())
+                join card in cardTable.Query()
+                on timeEntry.DomId equals card.DomId
+                orderby timeEntry.Month, card.Epic, card.ListIndex, card.Name, timeEntry.UserName
+                select new JoinModel()
+                {
+                    Month = timeEntry.Month,
+                    Epic = card.Epic,
+                    ListIndex = card.ListIndex,
+                    ListName = card.ListName,
+                    DomId = timeEntry.DomId,
+                    Name = card.Name,
+                    UserName = timeEntry.UserName,
+                    Billable = ((decimal)timeEntry.Billable) / 100,
+                    Invoice = card.Invoice
+                };
+
+            var housekeeping =
+                from timeEntry in GroupByMonth(timeEntryTable.Query())
+                where timeEntry.Housekeeping != null && timeEntry.Month > new System.DateTime(2015, 6, 30)
+                select new JoinModel()
+                {
+                    Month = timeEntry.Month,
+                    Epic = "Housekeeping",
+                    ListIndex = null,
+                    ListName = null,
+                    DomId = null,
+                    Name = timeEntry.Housekeeping,
+                    UserName = timeEntry.UserName,
+                    Billable = ((decimal)timeEntry.Billable) / 100,
+                    Invoice = timeEntry.Month
+                };
+
+
+
+            var result = stories.Concat(housekeeping);
+
+            return View(result);
+        }
+
+        public ActionResult Trello()
+        {
+            TableClient client = GetNewTableClient();
+
+            CardTable table = new CardTable(client);
+
+            var result = table.Query();
+
+            return View(result);
+        }
+
+        public ActionResult Toggl()
+        {
+            TableClient client = GetNewTableClient();
+
+            TimeEntryTable table = new TimeEntryTable(client);
+
+            var result = GroupByMonth(table.Query());
+
+            return View(result);
+        }
+
+        public ActionResult Orphans()
+        {
+            TableClient client = GetNewTableClient();
+
+            TimeEntryTable table = new TimeEntryTable(client);
+
+            var orphans =
+                from timeEntry in table.Query()
+                where
+                    timeEntry.Housekeeping == null &&
+                    timeEntry.DomId == null &&
+                    timeEntry.Month > new System.DateTime(2015, 6, 30)
+                orderby timeEntry.Month
+                select new TimeEntryEntity()
+                {
+                    Month = timeEntry.Month,
+                    Housekeeping = timeEntry.Housekeeping,
+                    UserName = timeEntry.UserName,
+                    Billable = timeEntry.Billable,
+                    TaskId = timeEntry.TaskId,
+                };
+
+            return View("Toggl", orphans);
+        }
+
+        private static IEnumerable<TimeEntryEntity> GroupByMonth(IEnumerable<TimeEntryEntity> query)
+        {
+            var result =
+                from e in query
+                group e by new
+                {
+                    e.Month,
+                    e.UserName,
+                    e.DomId,
+                    e.Housekeeping
+
+                } into g
+                select new TimeEntryEntity()
+                {
+                    Month = g.Key.Month,
+                    UserName = g.Key.UserName,
+                    DomId = g.Key.DomId,
+                    Housekeeping = g.Key.Housekeeping,
+                    Billable = g.Sum(e => e.Billable)
+                };
+
+            return result;
+        }
+
+        private static TableClient GetNewTableClient()
+        {
+            var azureConnectionString = ConfigurationManager.AppSettings["azureConnectionString"];
+
+            return new TableClient(azureConnectionString);
+        }
+    }
+}
