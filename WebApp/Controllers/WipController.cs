1aea165b82e41c88425d83d347d315f0726118fe
gothandy <gothandy@hotmail.com>
2015-12-22 06:42:14 +0000

    Wip report in place, no billable data yet.

---
+++ WebApp/Controllers/WipController.cs
@@ -0,0 +1,41 @@
+﻿using System.Configuration;
+using System.Linq;
+using System.Web.Mvc;
+using Vincente.Azure;
+using Vincente.Azure.Tables;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class WipController : Controller
+    {
+        // GET: Wip
+        public ActionResult Index()
+        {
+            var azureConnectionString = ConfigurationManager.AppSettings["azureConnectionString"];
+
+            var client = new TableClient(azureConnectionString);
+
+            CardTable cardTable = new CardTable(client);
+
+            var results =
+                from card in cardTable.Query()
+                group card by new
+                {
+                    card.ListIndex,
+                    card.ListName
+                }
+                into list
+                orderby list.Key.ListIndex
+                select new ListModel()
+                {
+                    ListIndex = list.Key.ListIndex,
+                    ListName = list.Key.ListName,
+                    Billable = 0
+                };
+                
+
+            return View(results);
+        }
+    }
+}
---
+++ WebApp/Models/ListModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class ListModel
+    {
+        public int? ListIndex { get; set; }
+        public string ListName { get; set; }
+        public decimal? Billable { get; set; }
+    }
+}
