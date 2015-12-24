5c05f5ab3066ef37aa4dc3812235d919825f7a9a
gothandy <gothandy@hotmail.com>
2015-12-24 14:36:16 +0000

    Starting to refactor way Toggl gets called and stored in Azure.

--- TogglConsoleApp/Program.cs
+++ TogglConsoleApp/Program.cs
@@ -18,73 +18,77 @@ namespace TogglConsoleApp
             var togglClientId = 15242883;
 
             var azureTableClient = new Vincente.Azure.TableClient(azureConnectionString);
+            TimeEntryTable azureTimeEntryTable = new TimeEntryTable(azureTableClient);
 
-            TogglToAzure(togglApiKey, togglWorkspaceId, togglClientId, azureTableClient);
+            var azureTableExists = azureTimeEntryTable.Exists();
+
+            if (!azureTableExists) azureTimeEntryTable.Create();
+
+            List<ReportTimeEntry> togglTimeEntries;
+
+            if (azureTableExists)
+            {
+                togglTimeEntries = TogglFromLastMonth(togglApiKey, togglWorkspaceId, togglClientId);
+            }
+            else
+            {
+                
+                togglTimeEntries = TogglFromJan2015(togglApiKey, togglWorkspaceId, togglClientId);
+            }
+
+            TogglToAzure2(azureTimeEntryTable, togglTimeEntries);
 
         }
 
         private static string CheckAndGetAppSettings(string name)
         {
             var value = ConfigurationManager.AppSettings[name];
-
             if (value == null) throw new ArgumentNullException(name);
-
             return value;
         }
 
-        private static void TogglToAzure(string togglApiKey, int togglWorkspaceId, int togglClientId, Vincente.Azure.TableClient azureTableClient)
-        {
-            TimeEntryTable table = new TimeEntryTable(azureTableClient);
 
-            if (table.Exists())
-            {
-                TogglToAzureLastMonth(table, togglApiKey, togglWorkspaceId, togglClientId);
-            }
-            else
-            {
-                table.Create();
-                TogglToAzureFromJan2015(table, togglApiKey, togglWorkspaceId, togglClientId);
-            }
-        }
-
-        private static void TogglToAzureFromJan2015(TimeEntryTable table, string togglApiKey, int togglWorkspaceId, int togglClientId)
+        private static List<ReportTimeEntry> TogglFromJan2015(string togglApiKey, int togglWorkspaceId, int togglClientId)
         {
             var until = DateTime.Now;
             var since = new DateTime(2015, 1, 1);
 
-            TogglToAzure(table, togglApiKey, togglWorkspaceId, togglClientId, until, since);
+            return GetTogglTimeEntries(togglApiKey, togglWorkspaceId, togglClientId, until, since);
         }
 
-        private static void TogglToAzureLastMonth(TimeEntryTable table, string togglApiKey, int togglWorkspaceId, int togglClientId)
+        private static List<ReportTimeEntry> TogglFromLastMonth(string togglApiKey, int togglWorkspaceId, int togglClientId)
         {
             var until = DateTime.Now;
             var since = until.AddMonths(-1);
 
-            TogglToAzure(table, togglApiKey, togglWorkspaceId, togglClientId, until, since);
+            return GetTogglTimeEntries(togglApiKey, togglWorkspaceId, togglClientId, until, since);
         }
 
-        private static void TogglToAzure(TimeEntryTable table, string apiKey, int workspaceId, int clientId, DateTime until, DateTime since)
+        private static List<ReportTimeEntry> GetTogglTimeEntries(string apiKey, int workspaceId, int clientId, DateTime until, DateTime since)
         {
             Console.Out.WriteLine("Toggl time entries from {0} to {1}", since, until);
 
             var workspace = new Vincente.Toggl.Workspace(apiKey, workspaceId);
 
-            List<ReportTimeEntry> reportTimeEntries = workspace.GetReportTimeEntries(clientId, since, until);
+            return workspace.GetReportTimeEntries(clientId, since, until);
+        }
 
-            Console.Out.WriteLine("{0} Time Entries Found.", reportTimeEntries.Count);
+        private static void TogglToAzure2(TimeEntryTable azureTimeEntry, List<ReportTimeEntry> togglTimeEntries)
+        {
+            Console.Out.WriteLine("{0} Time Entries Found.", togglTimeEntries.Count);
 
-            foreach (ReportTimeEntry timeEntry in reportTimeEntries)
+            foreach (ReportTimeEntry timeEntry in togglTimeEntries)
             {
                 TimeEntryEntity entity = new TimeEntryEntity(
                     timeEntry.Start, timeEntry.Id, timeEntry.ProjectId, timeEntry.TaskId, timeEntry.TaskName, timeEntry.UserName, timeEntry.Billable);
 
-                table.BatchInsertOrReplace(entity);
+                azureTimeEntry.BatchInsertOrReplace(entity);
 
                 //Use for testing to remove batch complexity.
                 //table.InsertOrReplace(entity);
             }
 
-            table.ExecuteBatch();
+            azureTimeEntry.ExecuteBatch();
         }
     }
 }
