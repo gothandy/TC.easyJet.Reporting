9caf55617ba56e8a7781b099b7328c0882ed052c
gothandy <gothandy@hotmail.com>
2015-12-24 08:15:00 +0000

    Getting timestamps from azure tables.

--- WebApp/Controllers/DefaultController.cs
+++ WebApp/Controllers/DefaultController.cs
@@ -1,29 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Web;
 using System.Web.Mvc;
+using Vincente.Azure;
+using Vincente.Azure.Tables;
 
 namespace WebApp.Controllers
 {
     public class DefaultController : Controller
     {
+        private TableClient tableClient;
+        public DefaultController(TableClient tableClient)
+        {
+            this.tableClient = tableClient;
+        }
+
         // GET: Default
         public ActionResult Index()
         {
+            ViewBag.BuildDateTime = GetBuildDateTime();
+            ViewBag.LatestCard = GetCardLatestTimestamp();
+            ViewBag.LatestTimeEntry = GetTimeEntryLatestTimestamp();
+
+            return View();
+        }
+
+        private DateTime GetCardLatestTimestamp()
+        {
+            var table = new CardTable(tableClient);
+
+            var latest =
+                from e in table.Query()
+                group e by 1 into g
+                select new
+                {
+                    Latest = g.Max(e => e.Timestamp)
+                };
+
+            return latest.First().Latest.LocalDateTime;
+        }
+
+        private DateTime GetTimeEntryLatestTimestamp()
+        {
+            var table = new TimeEntryTable(tableClient);
+
+            var latest =
+                from e in table.Query()
+                group e by 1 into g
+                select new
+                {
+                    Latest = g.Max(e => e.Timestamp)
+                };
+
+            return latest.First().Latest.LocalDateTime;
+        }
+
+        private DateTime GetBuildDateTime()
+        {
             var entryAssembly = Assembly.GetAssembly(typeof(WebApp.MvcApplication));
-
             var assemblyName = entryAssembly.GetName();
-
             var version = assemblyName.Version;
-
-            ViewBag.BuildDateTime = new DateTime(2000, 1, 1).Add(
+            var timeSpan =
                 new TimeSpan(
                     TimeSpan.TicksPerDay * version.Build + // days since 1 January 2000
-                    TimeSpan.TicksPerSecond * 2 * version.Revision)); // seconds since midnight, (multiply by 2 to get original)
-            
-            return View();
+                    TimeSpan.TicksPerSecond * 2 * version.Revision); // seconds since midnight, (multiply by 2 to get original)
+
+            return new DateTime(2000, 1, 1).Add(timeSpan); 
         }
     }
 }
