8ed1ba885cf46ff8426612970a3d95c2aedad349
gothandy <gothandy@hotmail.com>
2015-12-24 07:50:12 +0000

    Show build time on the homepage.

--- WebApp/Controllers/DefaultController.cs
+++ WebApp/Controllers/DefaultController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,6 +12,17 @@ namespace WebApp.Controllers
         // GET: Default
         public ActionResult Index()
         {
+            var entryAssembly = Assembly.GetAssembly(typeof(WebApp.MvcApplication));
+
+            var assemblyName = entryAssembly.GetName();
+
+            var version = assemblyName.Version;
+
+            ViewBag.BuildDateTime = new DateTime(2000, 1, 1).Add(
+                new TimeSpan(
+                    TimeSpan.TicksPerDay * version.Build + // days since 1 January 2000
+                    TimeSpan.TicksPerSecond * 2 * version.Revision)); // seconds since midnight, (multiply by 2 to get original)
+            
             return View();
         }
     }
