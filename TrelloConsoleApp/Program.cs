c0ceb5818c695fe5145ccdf52ccbb574f2d40483
gothandy <gothandy@hotmail.com>
2015-12-25 14:35:10 +0000

    Refactored Trello app to match Toggl.

--- TrelloConsoleApp/Program.cs
+++ TrelloConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using Vincente.Azure.Entities;
 using Vincente.Azure.Tables;
+using Vincente.Data.Tables;
 using Vincente.Formula;
 using Vincente.Trello.DataObjects;
 
@@ -21,55 +22,26 @@ namespace TrelloConsoleApp
             var trelloBoardId = "5596a7b7ac88c077383d281c";
             var trelloWorkspace = new Vincente.Trello.Workspace(trelloKey, trelloToken, trelloBoardId);
 
-            TrelloToAzure(azureTableClient, trelloWorkspace);
-        }
-
-        private static string CheckAndGetAppSettings(string name)
-        {
-            var value = ConfigurationManager.AppSettings[name];
-
-            if (value == null) throw new ArgumentNullException(name);
-
-            return value;
-        }
-
-        private static void TrelloToAzure(Vincente.Azure.TableClient tableClient, Vincente.Trello.Workspace workspace)
-        {
-            List<Card> cards = workspace.GetCards();
-            List<Label> labels = workspace.GetLabels();
-            List<List> lists = workspace.GetLists();
+            List<Card> cards = trelloWorkspace.GetCards();
+            List<Label> labels = trelloWorkspace.GetLabels();
+            List<List> lists = trelloWorkspace.GetLists();
 
             Console.Out.WriteLine("{0} Cards Found", cards.Count);
             Console.Out.WriteLine("{0} Labels Found", labels.Count);
             Console.Out.WriteLine("{0} Lists Found", lists.Count);
 
-            CardTable table = new CardTable(tableClient);
-            table.CreateIfNotExists();
+            CardTable cardTable = new CardTable(azureTableClient);
 
-            foreach (Card card in cards)
-            {
-                var list = List.GetList(card.IdList, lists);
-                var listIndex = lists.IndexOf(list);
-                var listName = list.Name;
-                var nameLabels = Label.GetNameLabels(card.IdLabels, labels);
-                var cardName = card.Name;
-                var cardId = card.Id;
+            cardTable.CreateIfNotExists();
 
-                var data = new Vincente.Data.Entities.Card()
-                {
-                    DomId = FromName.GetDomID(cardName),
-                    Id = card.Id,
-                    ListIndex = listIndex,
-                    ListName = listName,
-                    Name = FromName.GetShortName(cardName),
-                    Epic = FromLabels.GetEpic(nameLabels),
-                    Invoice = FromLabels.GetInvoice(nameLabels, listName)
-                };
+            TrelloToData.Execute(cardTable, cards, labels, lists);
+        }
 
-                table.BatchInsertOrReplace(data);
-            }
-
-            table.ExecuteBatch();
+        private static string CheckAndGetAppSettings(string name)
+        {
+            var value = ConfigurationManager.AppSettings[name];
+            if (value == null) throw new ArgumentNullException(name);
+            return value;
         }
     }
 }
---
+++ TrelloConsoleApp/TrelloToData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Vincente.Data.Tables;
+using Vincente.Formula;
+using Vincente.Trello.DataObjects;
+
+namespace TrelloConsoleApp
+{
+    public class TrelloToData
+    {
+        public static void Execute(ITable<Vincente.Data.Entities.Card> table, List<Card> cards, List<Label> labels, List<List> lists)
+        {
+            foreach (Card card in cards)
+            {
+                Vincente.Data.Entities.Card data = GetDataFromTrello(card, labels, lists);
+
+                table.BatchInsertOrReplace(data);
+            }
+
+            table.ExecuteBatch();
+        }
+
+        private static Vincente.Data.Entities.Card GetDataFromTrello(Card card, List<Label> labels, List<List> lists)
+        {
+            var list = List.GetList(card.IdList, lists);
+            var listIndex = lists.IndexOf(list);
+            var listName = list.Name;
+            var nameLabels = Label.GetNameLabels(card.IdLabels, labels);
+            var cardName = card.Name;
+            var cardId = card.Id;
+
+            return
+                new Vincente.Data.Entities.Card()
+                {
+                    DomId = FromName.GetDomID(cardName),
+                    Id = card.Id,
+                    ListIndex = listIndex,
+                    ListName = listName,
+                    Name = FromName.GetShortName(cardName),
+                    Epic = FromLabels.GetEpic(nameLabels),
+                    Invoice = FromLabels.GetInvoice(nameLabels, listName)
+                };
+        }
+    }
+}
