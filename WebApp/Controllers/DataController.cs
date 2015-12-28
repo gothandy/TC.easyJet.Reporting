15d1fc3b3811d229baf4fe64f3927c4f31cbea8e
gothandy <gothandy@hotmail.com>
2015-12-28 17:27:47 +0000

    Updated WipController.

--- WebApp/Controllers/DataController.cs
+++ WebApp/Controllers/DataController.cs
@@ -1,10 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Web.Mvc;
-using Vincente.Data.Entities;
 using Vincente.Data.Tables;
 using Vincente.WebApp.Models;
-using WebApp.Models;
 
 namespace Vincente.WebApp.Controllers
 {
--- WebApp/Controllers/WipController.cs
+++ WebApp/Controllers/WipController.cs
@@ -1,78 +1,69 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Web.Mvc;
-using WebApp.Models;
+using Vincente.Data.Entities;
+using Vincente.Data.Tables;
+using Vincente.WebApp.Models;
 
 namespace WebApp.Controllers
 {
     public class WipController : Controller
     {
-        private JoinClient joinClient;
+        private CardsWithTime cardsWithTime;
 
-        public WipController(JoinClient joinClient)
+        public WipController(ModelParameters modelParameters)
         {
-            this.joinClient = joinClient;
+            var timeEntriesByMonth = new TimeEntriesByMonth(modelParameters.TimeEntry);
+            cardsWithTime = new CardsWithTime(modelParameters.Card, timeEntriesByMonth);
         }
 
         // GET: Wip
         public ActionResult ByList()
         {
-            IEnumerable<JoinModel> data = joinClient.GetWipData();
+            var data =
+               from e in cardsWithTime.Query()
+               where e.Invoice == null
+               group e by new
+               {
+                   e.ListIndex,
+                   e.ListName
+               }
+               into g
+               orderby g.Key.ListIndex
+               select new CardWithTime()
+               {
+                   ListIndex = g.Key.ListIndex,
+                   ListName = g.Key.ListName,
+                   Billable = g.Sum(e => e.Billable)
+               };
 
-            return View(AllWip(data));
+            return View(data);
         }
 
+        // GET: Wip/1
         public ActionResult Detail(int? list)
         {
-            IEnumerable<JoinModel> data = joinClient.GetWipData();
+            var data =
+                from e in cardsWithTime.Query()
+                    where e.Invoice == null && e.ListIndex == list
+                    group e by new
+                    {
+                        e.CardId,
+                        e.ListName,
+                        e.Epic,
+                        e.DomId,
+                        e.Name
+                    } into g
+                    select new CardWithTime()
+                    {
+                        CardId = g.Key.CardId,
+                        ListName = g.Key.ListName,
+                        Epic = g.Key.Epic,
+                        DomId = g.Key.DomId,
+                        Name = g.Key.Name,
+                        Billable = g.Sum(e => e.Billable)
+                    };
 
-            return View(GetByList(data, list));
-        }
-
-        private static IEnumerable<JoinModel> GetByList(IEnumerable<JoinModel> data, int? list)
-        {
-            return from e in data
-                   where e.ListIndex == list
-                   group e by new
-                   {
-                       e.CardId,
-                       e.ListName,
-                       e.Epic,
-                       e.DomId,
-                       e.Name
-                   } into g
-                   select new JoinModel()
-                   {
-                       CardId = g.Key.CardId,
-                       ListName = g.Key.ListName,
-                       Epic = g.Key.Epic,
-                       DomId = g.Key.DomId,
-                       Name = g.Key.Name,
-                       Billable = g.Sum(e => e.Billable)
-                   };
-        }
-
-
-
-        private IEnumerable<JoinModel> AllWip(IEnumerable<JoinModel> data)
-        {
-            var result =
-                from e in data
-                
-                group e by new
-                {
-                    e.ListIndex,
-                    e.ListName
-                } into g
-                orderby g.Key.ListIndex
-                select new JoinModel()
-                {
-                    ListIndex = g.Key.ListIndex,
-                    ListName = g.Key.ListName,
-                    Billable = g.Sum(e => e.Billable)
-                };
-
-            return result;
+            return View(data);
         }
     }
 }
