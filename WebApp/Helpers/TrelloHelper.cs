dfad64f8dc9a646a477d3b1163437770139046e8
gothandy <gothandy@hotmail.com>
2016-01-03 14:59:59 +0000

    Trello helper to link from name consistently.

---
+++ WebApp/Helpers/TrelloHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+
+namespace Vincente.WebApp.Helpers
+{
+    public class TrelloHelper
+    {
+        public static HtmlString CardLink(string cardId, string text)
+        {
+            return new HtmlString(string.Format(
+                "<a target=\"_blank\" href=\"https://trello.com/c/{0}\">{1}</a>",
+                cardId, text));
+        }
+    }
+}
