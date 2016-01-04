c6473376a5813487ff2295ef797edc88d2e97acc
gothandy <gothandy@hotmail.com>
2016-01-04 13:01:58 +0000

    Added User reports

--- WebApp/Controllers/UserController.cs
+++ WebApp/Controllers/UserController.cs
@@ -1,10 +1,10 @@
-﻿using System.Web.Mvc;
-using Vincente.Data.Interfaces;
-using Vincente.Data.Tables;
-using System.Linq;
-using Vincente.Data.Entities;
+﻿using System;
 using System.Collections.Generic;
-using System;
+using System.Linq;
+using System.Web.Mvc;
+using Vincente.Data.Entities;
+using Vincente.Data.Interfaces;
+using Vincente.WebApp.Models;
 
 namespace Vincente.WebApp.Controllers
 {
@@ -19,7 +19,41 @@ namespace Vincente.WebApp.Controllers
         // GET: User
         public ActionResult Summary()
         {
-            return View();
+            var result =
+               from e in timeEntries
+               where e.Start > new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddMonths(-1)
+            group e by new
+               {
+                   e.UserName
+               }
+               into g
+               orderby g.Key.UserName
+               select new UserModel()
+               {
+                   UserName = g.Key.UserName,
+                   LastMonth = g.Sum(e => sumMonth(e.Start, e.Billable, -1)),
+                   ThisMonth = g.Sum(e => sumMonth(e.Start, e.Billable, 0)),
+                   Yesterday = g.Sum(e => sumDay(e.Start, e.Billable, -1)),
+                   Today = g.Sum(e => sumDay(e.Start, e.Billable, 0)),
+                   Total = g.Sum(e => e.Billable)
+               };
+
+            return View(result);
         }
+
+        private decimal sumMonth(DateTime start, decimal billable, int months)
+        {
+            return (getMonth(start) == getMonth(DateTime.Now).AddMonths(months)) ? billable :  0;
+        }
+
+        private decimal sumDay(DateTime start, decimal billable, int days)
+        {
+            return (getDay(start) == getDay(DateTime.Now).AddDays(days)) ? billable : 0;
+        }
+
+        private DateTime getMonth(DateTime d) { return new DateTime(d.Year, d.Month, 1); }
+
+        private DateTime getDay(DateTime d) { return new DateTime(d.Year, d.Month, d.Day); }
+
     }
 }
---
+++ WebApp/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vincente.WebApp.Models
+{
+    public class UserModel
+    {
+        public string UserName { get; set; }
+        public decimal LastMonth { get; set; }
+        public decimal ThisMonth { get; set; }
+        public decimal LastWeek { get; set; }
+        public decimal ThisWeek { get; set; }
+        public decimal Yesterday { get; set; }
+        public decimal Today { get; set; }
+        public decimal Total { get; set; }
+    }
+}
