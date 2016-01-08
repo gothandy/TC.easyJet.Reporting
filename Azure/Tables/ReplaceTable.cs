e236a3e51f5afe8cb380de949a4f96a32648fb1c
gothandy <gothandy@hotmail.com>
2016-01-08 08:45:45 +0000

    Untracked file.

---
+++ Azure/Tables/ReplaceTable.cs
@@ -0,0 +1,11 @@
+﻿using Gothandy.Tables.AzureBlobs;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Vincente.Data.Entities;
+
+namespace Vincente.Azure.Tables
+{
+    public class ReplaceTable : AzureBlob<Team>
+    {
+        public ReplaceTable(CloudBlockBlob blob) : base(blob) { }
+    }
+}
