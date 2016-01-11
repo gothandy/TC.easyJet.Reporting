88d38fca811d871b03e32e011a0007a04e01a6de
gothandy <gothandy@hotmail.com>
2016-01-11 11:50:59 +0000

    Link to monthly summary report.

--- WebApp/Helpers/TogglHelper.cs
+++ WebApp/Helpers/TogglHelper.cs
@@ -10,13 +10,24 @@ namespace Vincente.WebApp.Helpers
         private const long togglWorkspaceId = 605632;
         private const long togglClientId = 15242883;
 
+        //https://www.toggl.com/app/reports/summary/605632/period/prevMonth /clients/15242883/billable/both
+
+        public static HtmlString SummaryReport(DateTime from, string text)
+        {
+            var to = from.AddMonths(1).AddDays(-1);
+
+            return new HtmlString(string.Format(
+                "<a target=\"blank\" href=\"{0}/clients/{1}/billable/both\">{2}</a>",
+                BaseUrl("summary", from, to), togglClientId, text));
+        }
+        
         public static HtmlString SummaryReport(List<long> taskIds)
         {
             if (taskIds == null) return null;
 
             return new HtmlString(string.Format(
-                "<a target=\"blank\" href=\"https://www.toggl.com/app/reports/summary/605632/period/prevYear/clients/15242883/tasks/{0}/billable/both\">{0}</a>",
-                string.Join(",", taskIds)));
+                "<a target=\"blank\" href=\"{0}/clients/{1}/tasks/{2}/billable/both\">{0}</a>",
+                BaseUrlYear(), togglClientId, string.Join(",", taskIds)));
         }
 
         public static HtmlString DetailedReport(long? taskId, string text)
@@ -42,17 +53,20 @@ namespace Vincente.WebApp.Helpers
 
             return new HtmlString(string.Format(
                 "<a target=\"blank\" href=\"{0}/users/{1}/clients/{2}/tasks/0/billable/both\">{3}</a>",
-                BaseUrl(), userId, togglClientId, text));
+                BaseUrlYear(), userId, togglClientId, text));
         }
 
         //www.toggl.com/app/reports/detailed/605632/from/2015-12-17/to/2016-01-21
-        private static string BaseUrl()
+        private static string BaseUrlYear()
+        {
+            return BaseUrl("detailed", DateTime.Now.AddYears(-1), DateTime.Now);
+        }
+
+        private static string BaseUrl(string type, DateTime from, DateTime to)
         {
             return string.Format(
-                "https://www.toggl.com/app/reports/detailed/{0}/from/{1:yyyy-MM-dd}/to/{2:yyyy-MM-dd}",
-                togglWorkspaceId,
-                DateTime.Now.AddYears(-1),
-                DateTime.Now);
+                "https://www.toggl.com/app/reports/{0}/{1}/from/{2:yyyy-MM-dd}/to/{3:yyyy-MM-dd}",
+                type, togglWorkspaceId, from, to);
         }
     }
 }
