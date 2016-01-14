a7ed43a91724247ba898af0191da3798879502f8
gothandy <gothandy@hotmail.com>
2016-01-14 12:45:34 +0000

    Helper config.

--- WebApp/Helpers/TogglHelper.cs
+++ WebApp/Helpers/TogglHelper.cs
@@ -2,32 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Vincente.Config;
 
 namespace Vincente.WebApp.Helpers
 {
     public class TogglHelper
     {
-        private const long togglWorkspaceId = 605632;
-        private const long togglClientId = 15242883;
-
-        //https://www.toggl.com/app/reports/summary/605632/period/prevMonth /clients/15242883/billable/both
 
         public static HtmlString SummaryReport(DateTime from, string text)
         {
+            var config = ConfigBuilder.Build();
             var to = from.AddMonths(1).AddDays(-1);
 
             return new HtmlString(string.Format(
                 "<a target=\"blank\" href=\"{0}/clients/{1}/billable/both\">{2}</a>",
-                BaseUrl("summary", from, to), togglClientId, text));
+                BaseUrl(config.togglWorkspaceId, "summary", from, to), config.togglClientId, text));
         }
         
         public static HtmlString SummaryReport(List<long> taskIds)
         {
+            var config = ConfigBuilder.Build();
             if (taskIds == null) return null;
 
             return new HtmlString(string.Format(
                 "<a target=\"blank\" href=\"{0}/clients/{1}/tasks/{2}/billable/both\">{0}</a>",
-                BaseUrlYear(), togglClientId, string.Join(",", taskIds)));
+                BaseUrlYear(config.togglWorkspaceId), config.togglClientId, string.Join(",", taskIds)));
         }
 
         public static HtmlString DetailedReport(long? taskId, string text)
@@ -49,20 +48,21 @@ namespace Vincente.WebApp.Helpers
         // /users/960313/clients/15242883/tasks/0/billable/both
         public static HtmlString DetailedUserNoTask(long? userId, string text)
         {
+            var config = ConfigBuilder.Build();
             if (!userId.HasValue) return new HtmlString(text);
 
             return new HtmlString(string.Format(
                 "<a target=\"blank\" href=\"{0}/users/{1}/clients/{2}/tasks/0/billable/both\">{3}</a>",
-                BaseUrlYear(), userId, togglClientId, text));
+                BaseUrlYear(config.togglWorkspaceId), userId, config.togglClientId, text));
         }
 
         //www.toggl.com/app/reports/detailed/605632/from/2015-12-17/to/2016-01-21
-        private static string BaseUrlYear()
+        private static string BaseUrlYear(int togglWorkspaceId)
         {
-            return BaseUrl("detailed", DateTime.Now.AddYears(-1), DateTime.Now);
+            return BaseUrl(togglWorkspaceId, "detailed", DateTime.Now.AddYears(-1), DateTime.Now);
         }
 
-        private static string BaseUrl(string type, DateTime from, DateTime to)
+        private static string BaseUrl(int togglWorkspaceId, string type, DateTime from, DateTime to)
         {
             return string.Format(
                 "https://www.toggl.com/app/reports/{0}/{1}/from/{2:yyyy-MM-dd}/to/{3:yyyy-MM-dd}",
