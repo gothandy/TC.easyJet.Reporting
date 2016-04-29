ac89cb04365e018dc10aa97324f3accd33bbca39
gothandy <gothandy@hotmail.com>
2016-04-29 15:13:35 +0100

    Move main into class.

--- TogglConsoleApp/Program.cs
+++ TogglConsoleApp/Program.cs
@@ -1,12 +1,7 @@
 ﻿using Gothandy.StartUp;
-using Gothandy.Tables.Bulk;
 using Microsoft.WindowsAzure.Storage;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using Vincente.Azure.Tables;
-using Vincente.Data.Entities;
-using Gothandy.Toggl.DataObjects;
 using Vincente.Config;
 
 namespace TogglConsoleApp
@@ -31,69 +26,11 @@ namespace TogglConsoleApp
             var azureTeamTable = new ReplaceTable(azureTeamListBlob);
 
             var togglWorkspace = new Gothandy.Toggl.Workspace(config.togglApiKey, config.togglWorkspaceId);
+
+            var togglToTime = new TogglToTime(config.togglClientId, azureTimeEntryTable, azureTeamTable, togglWorkspace);
             #endregion
-            
-            var azureTeamList = azureTeamTable.Query().ToList();
-            Console.Out.WriteLine("{0} Team List Items", azureTeamList.Count);
 
-            var createTable = !azureTimeEntryTable.Exists();
-            if (createTable) azureTimeEntryTable.Create();
-            var getAll = false; //createTable;
-
-            var togglTimeEntries = GetTogglTimeEntries(togglWorkspace, config.togglClientId, getAll);
-
-            Console.Out.WriteLine("{0} Time Entries Found.", togglTimeEntries.Count);
-
-            var timeEntryTable = new TimeEntryTable(azureTimeEntryTable);
-
-            var newTimeEntries = TogglToData.Execute(timeEntryTable, togglTimeEntries, azureTeamList);
-            var oldTimeEntries = GetOldTimeEntries(timeEntryTable, getAll);
-
-            Console.Out.WriteLine("{0} New Count.", newTimeEntries.Count);
-            Console.Out.WriteLine("{0} Old Count.", oldTimeEntries.Count);
-
-            var operations = new Operations<TimeEntry>(timeEntryTable);
-
-            var results = operations.BatchCompare(oldTimeEntries, newTimeEntries);
-
-            Console.Out.WriteLine("{0} Time Entries Inserted", results.Inserted);
-            Console.Out.WriteLine("{0} Time Entries Ignored", results.Ignored);
-            Console.Out.WriteLine("{0} Time Entries Replaced", results.Replaced);
-            Console.Out.WriteLine("{0} Time Entries Deleted", results.Deleted);
-        }
-
-
-        private static List<ReportTimeEntry> GetTogglTimeEntries(Gothandy.Toggl.Workspace togglWorkspace, int clientId, bool getAll)
-        {
-            DateTime until = GetUntil();
-            DateTime since = GetSince(getAll, until);
-
-            Console.Out.WriteLine("Toggl time entries from {0} to {1}", since, until);
-
-            var table = new Gothandy.Toggl.Tables.TimeEntryTable(togglWorkspace);
-
-            return table.GetReportTimeEntries(clientId, since, until);
-        }
-
-        private static List<TimeEntry> GetOldTimeEntries(TimeEntryTable table, bool getAll)
-        {
-            DateTime until = GetUntil();
-            DateTime since = GetSince(getAll, until);
-
-            return
-                (from timeEntry in table.Query()
-                 where timeEntry.Start >= since
-                 select timeEntry).ToList();
-        }
-
-        private static DateTime GetSince(bool getAll, DateTime until)
-        {
-            return getAll ? until.AddYears(-1) : until.AddMonths(-1);
-        }
-
-        private static DateTime GetUntil()
-        {
-            return new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day).AddDays(1);
+            togglToTime.Execute();
         }
     }
 }
---
+++ TogglConsoleApp/TogglToTime.cs
@@ -0,0 +1,97 @@
+﻿using Gothandy.Tables.Bulk;
+using Gothandy.Toggl.DataObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Vincente.Azure.Tables;
+using Vincente.Data.Entities;
+using Gothandy.Toggl;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace TogglConsoleApp
+{
+    public class TogglToTime
+    {
+        private int togglClientId;
+        private CloudTable azureTimeEntryTable;
+        private ReplaceTable azureTeamTable;
+        private Workspace togglWorkspace;
+
+        public TogglToTime(int togglClientId, CloudTable azureTimeEntryTable, ReplaceTable azureTeamTable, Workspace togglWorkspace)
+        {
+            this.togglClientId = togglClientId;
+            this.azureTimeEntryTable = azureTimeEntryTable;
+            this.azureTeamTable = azureTeamTable;
+            this.togglWorkspace = togglWorkspace;
+        }
+
+        public void Execute()
+        {
+            var azureTeamList = azureTeamTable.Query().ToList();
+            Console.Out.WriteLine("{0} Team List Items", azureTeamList.Count);
+
+            var createTable = !azureTimeEntryTable.Exists();
+            if (createTable) azureTimeEntryTable.Create();
+            var getAll = false; //createTable;
+
+            var togglTimeEntries = GetTogglTimeEntries(togglWorkspace, togglClientId, getAll);
+
+            Console.Out.WriteLine("{0} Time Entries Found.", togglTimeEntries.Count);
+
+            var timeEntryTable = new TimeEntryTable(azureTimeEntryTable);
+
+            var newTimeEntries = TogglToData.Execute(timeEntryTable, togglTimeEntries, azureTeamList);
+            var oldTimeEntries = GetOldTimeEntries(timeEntryTable, getAll);
+
+            Console.Out.WriteLine("{0} New Count.", newTimeEntries.Count);
+            Console.Out.WriteLine("{0} Old Count.", oldTimeEntries.Count);
+
+            var operations = new Operations<TimeEntry>(timeEntryTable);
+
+            var results = operations.BatchCompare(oldTimeEntries, newTimeEntries);
+
+            ConsoleOutResults(results);
+        }
+
+        private static void ConsoleOutResults(Results results)
+        {
+            Console.Out.WriteLine("{0} Time Entries Inserted", results.Inserted);
+            Console.Out.WriteLine("{0} Time Entries Ignored", results.Ignored);
+            Console.Out.WriteLine("{0} Time Entries Replaced", results.Replaced);
+            Console.Out.WriteLine("{0} Time Entries Deleted", results.Deleted);
+        }
+
+        private static List<ReportTimeEntry> GetTogglTimeEntries(Gothandy.Toggl.Workspace togglWorkspace, int clientId, bool getAll)
+        {
+            DateTime until = GetUntil();
+            DateTime since = GetSince(getAll, until);
+
+            Console.Out.WriteLine("Toggl time entries from {0} to {1}", since, until);
+
+            var table = new Gothandy.Toggl.Tables.TimeEntryTable(togglWorkspace);
+
+            return table.GetReportTimeEntries(clientId, since, until);
+        }
+
+        private static List<TimeEntry> GetOldTimeEntries(TimeEntryTable table, bool getAll)
+        {
+            DateTime until = GetUntil();
+            DateTime since = GetSince(getAll, until);
+
+            return
+                (from timeEntry in table.Query()
+                 where timeEntry.Start >= since
+                 select timeEntry).ToList();
+        }
+
+        private static DateTime GetSince(bool getAll, DateTime until)
+        {
+            return getAll ? until.AddYears(-1) : until.AddMonths(-1);
+        }
+
+        private static DateTime GetUntil()
+        {
+            return new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day).AddDays(1);
+        }
+    }
+}
